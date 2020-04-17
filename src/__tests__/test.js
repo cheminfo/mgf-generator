722f126a50601133cf0ab4ec3a480d35be32fb2a
Océane Patiny <oceane@patiny.com>
2020-04-17 11:11:27 +0200

    feat: create generateMGF method and its test

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,7 +1,33 @@
-import { myModule } from '..';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
-describe('test myModule', () => {
-  it('should return 42', () => {
-    expect(myModule()).toEqual(42);
+import { parse } from 'mgf-parser';
+
+import generateMGF from '..';
+
+let data = readFileSync(join(__dirname, './data/test.mgf'), 'utf8');
+let parsedData = parse(data);
+
+let longDataRaw = readFileSync(join(__dirname, './data/longData.mgf'), 'utf8');
+let longData = parse(longDataRaw);
+
+let twoEntriesRaw = readFileSync(
+  join(__dirname, './data/twoEntries.mgf'),
+  'utf8',
+);
+let twoEntries = parse(twoEntriesRaw);
+
+describe('generateMGF', () => {
+  it('should return simple data', () => {
+    let result = generateMGF(parsedData);
+    expect(data).toBe(result);
+  });
+  it('should return two entries data', () => {
+    let result = generateMGF(twoEntries);
+    expect(twoEntriesRaw).toBe(result);
+  });
+  it('should return long data', () => {
+    let result = generateMGF(longData);
+    expect(longDataRaw).toBe(result);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
-/**
- * Returns a very important number
- * @return {number}
- */
-export function myModule() {
-  return 42;
+export default function generateMGF(parsedMGF, options = {}) {
+  const { lineDelimiter = '\n', spectrumDelimiter = ' ' } = options;
+
+  let data = [];
+
+  for (let entry of parsedMGF) {
+    // handle entry type at the beginning
+    data.push(`BEGIN ${entry.kind}`);
+
+    // handle metadata
+    for (let key in entry.meta) {
+      data.push(`${key}=${entry.meta[key]}`);
+    }
+
+    // handle spectrum
+    for (let i = 0; i < entry.data.x.length; i++) {
+      data.push(`${entry.data.x[i]}${spectrumDelimiter}${entry.data.y[i]}`);
+    }
+
+    // handle entry type at the end
+    data.push(`END ${entry.kind}`);
+  }
+
+  return data.join(lineDelimiter);
 }
